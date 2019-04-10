d7119244e64ea9a0f01c24dabe815279e12e13f8
α <mangertim@163.com>
2019-04-10 21:43:36 +0800

    step 4 of 10

--- src/node_robot_1.cpp
+++ src/node_robot_1.cpp
@@ -6,8 +6,8 @@
 
 bool handle_task(mini_multi_agent::task::Request &req, mini_multi_agent::task::Response &res)
 {
-  ROS_INFO("publish the task %s.", req.task_name.c_str());
-  res.feedback = "Task is to give " + req.robot_name + " to handle.";
+  ROS_INFO("publish the task %d.", req.task_ID);
+  res.feedback = "The task is to give " + req.robot_name + " to handle.";
   return true;    
 }
 
@@ -20,13 +20,14 @@ int main(int argc, char **argv)
   ros::NodeHandle nh;
 
   //自定义gps msg
-  mini_multi_agent::feedback msg;
-  msg.state = "ready";
+  // mini_multi_agent::feedback msg;
+  // msg.robot_ID = 1;
+  // msg.state = "ready";
 
   //创建publisher
-  ros::Publisher pub = nh.advertise<mini_multi_agent::feedback>("/agent_feedback", 1);
+  // ros::Publisher pub = nh.advertise<mini_multi_agent::feedback>("/agent_feedback", 1);
 
-  // //定义发布的频率 
+  // // //定义发布的频率 
   // ros::Rate loop_rate(1.0);
   // //循环发布msg
   // while (ros::ok())
