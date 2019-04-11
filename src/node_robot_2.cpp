9fbf36f31d31efbfca597716ecb95790dd5052bb
α <mangertim@163.com>
2019-04-11 22:21:36 +0800

    simplify code

--- src/node_robot_2.cpp
+++ src/node_robot_2.cpp
@@ -1,11 +1,10 @@
-//ROS头文件
 #include <ros/ros.h>
 #include <robot.h>
 
 int main(int argc, char **argv)
 {
-  //用于解析ROS参数，第三个参数为本节点名
   ros::init(argc, argv, "node_robot_2");
+  
   Robot robot_2(2, 3, 0.1);
   
   ros::spin(); 
